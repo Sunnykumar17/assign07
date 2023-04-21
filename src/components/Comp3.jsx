74029fa1bfb72bd9d0b84d9ad72989fa1ae5fe52
Sunny kumar <98548654+Sunnykumar17@users.noreply.github.com>
2023-04-21 17:40:56 +0530

    Add files via upload

---
+++ src/components/Comp3.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Comp3.css";
+const Comp3 = () => {
+  return (
+    <div className='card-container'>
+      <div className='card'>
+        <h2>Logo</h2>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Order</li>
+          <li>Contact</li>
+        </ul>
+        </div>
+        <div className='btn'>
+        <img src="https://images.unsplash.com/photo-1585409677983-0f6c41ca9c3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80" alt="img" className='img' />
+        </div>
+    </div>
+   
+  )
+}
+
+export default Comp3
